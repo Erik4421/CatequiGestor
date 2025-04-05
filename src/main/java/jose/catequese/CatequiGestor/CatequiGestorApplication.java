439c7e288557c3c5f2f12005032204685c610911
Erik Silva <oliveiraerik464@gmail.com>
2025-04-05 12:58:02 -0300

    Starting project

---
+++ src/main/java/jose/catequese/CatequiGestor/CatequiGestorApplication.java
@@ -0,0 +1,13 @@
+package jose.catequese.CatequiGestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatequiGestorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatequiGestorApplication.class, args);
+	}
+
+}
